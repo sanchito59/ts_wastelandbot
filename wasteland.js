ab715dfdca679a31ad596a7765f19cf55693bf24
Chris Sanchez <c.sanch7@gmail.com>
2020-03-18 15:28:48 -0700

    add API interface logic, Procfile, gitignore

---
+++ wasteland.js
@@ -0,0 +1,10 @@
+let Twit = require(‘twit’);
+let TwitterBot = require(‘node - twitterbot’).TwitterBot;
+let Bot = new TwitterBot({
+  consumer_key: process.env.BOT_CONSUMER_KEY,
+  consumer_secret: process.env.BOT_CONSUMER_SECRET,
+  access_token: process.env.BOT_ACCESS_TOKEN,
+  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+});
+
+Bot.tweet(phrase);
